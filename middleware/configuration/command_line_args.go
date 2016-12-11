27a30ad59a93c22962db76c69f49bc46b4a9e7f8
Marcin Tojek <tojek.m@gmail.com>
2016-12-11 17:06:58 +0100

    Change listening port to 9001

--- middleware/configuration/command_line_args.go
+++ middleware/configuration/command_line_args.go
@@ -3,7 +3,7 @@ package configuration
 import "flag"
 
 func FromCommandLineArgs() *ApplicationConfiguration {
-	hostPort := flag.String("hostPort", ":9000", "Host:port of the greenwall HTTP server")
+	hostPort := flag.String("hostPort", ":9001", "Host:port of the greenwall HTTP server")
 	staticDir := flag.String("staticDir", "frontend", "Path to frontend static resources")
 	flag.Parse()
 
