6c3e08fee76a2b21b53155bb90a3c560e5f3394d
Marcin Tojek <tojek.m@gmail.com>
2016-12-11 21:30:32 +0100

    Validate configuration

--- middleware/configuration/command_line_args.go
+++ middleware/configuration/command_line_args.go
@@ -1,14 +1,33 @@
 package configuration
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"os"
+	"path"
+)
+
+const indexFile = "index.html"
 
 func FromCommandLineArgs() *ApplicationConfiguration {
 	hostPort := flag.String("hostPort", ":9001", "Host:port of the greenwall HTTP server")
 	staticDir := flag.String("staticDir", "frontend", "Path to frontend static resources")
 	flag.Parse()
 
-	return &ApplicationConfiguration{
+	applicationConfiguration := &ApplicationConfiguration{
 		HostPort:  *hostPort,
 		StaticDir: *staticDir,
 	}
+
+	err := Validate(applicationConfiguration)
+	if err != nil {
+		log.Fatalf("Error occurred while validating configuration: %v", err)
+	}
+	return applicationConfiguration
+}
+
+func Validate(applicationConfiguration *ApplicationConfiguration) error {
+	indexFile := path.Join(applicationConfiguration.StaticDir, indexFile)
+	_, err := os.Stat(indexFile)
+	return err
 }
